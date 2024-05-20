c28618ca2474b4499b102b8a7e7be78043a6770c
Noractron <jonathan.joan.avila@gmail.com>
2024-05-20 14:15:43 -0500

    chore/add more commands to install libraries on all projects when exceute command ./start-all.ps1

--- start-all.ps1
+++ start-all.ps1
@@ -25,6 +25,21 @@ $antiFraudPath = Join-Path $currentDir "anti-fraud"
 $apiPath = Join-Path $currentDir "api"
 $transactionPath = Join-Path $currentDir "transaction"
 
+# Instalar dependencias en anti-fraud
+Write-Output "Installing dependencies for anti-fraud"
+cd $antiFraudPath
+npm install
+
+# Instalar dependencias en api
+Write-Output "Installing dependencies for api"
+cd $apiPath
+npm install
+
+# Instalar dependencias en transaction
+Write-Output "Installing dependencies for transaction"
+cd $transactionPath
+npm install
+
 Write-Output "Starting transaction (generate)"
 Start-Process powershell -ArgumentList "cd $transactionPath; npm run generate"
 
@@ -42,7 +57,5 @@ Start-Process powershell -ArgumentList "cd $apiPath; npm run start:dev"
 # Iniciar transaction en nuevas ventanas de PowerShell 
 Write-Output "Starting transaction (dev)"
 Start-Process powershell -ArgumentList "cd $transactionPath; npm run dev"
-
-
 
 Write-Output "All projects started"
